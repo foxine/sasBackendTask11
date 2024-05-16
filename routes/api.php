74584d8863768d473d12df3712ab715cd975deff
foxine <c.gc14@hotmail.com>
2024-05-16 01:08:18 -0300

    ALL FIXED!!!!

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,10 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ApiController;
 
+
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\StoreController;
+
 /*Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');*/
@@ -11,9 +15,15 @@ use App\Http\Controllers\Api\ApiController;
 Route::post('/register', [ApiController::class, 'register']);
 Route::post('/login', [ApiController::class, 'login']);
 
-/*Route::group([
+Route::group([
         'middleware'=>['auth:sanctum']
 ], function (){
-    Route::post('/books', BookController::class);
-});*/
+
+    Route::get('/books', [BookController::class, 'index']); // Listar todos os livros
+    Route::post('/books', [BookController::class, 'store']); // Criar um novo livro
+    Route::get('/books/{book}', [BookController::class, 'show']); // Exibir um livro específico
+    Route::put('/books/{book}', [BookController::class, 'update']); // Atualizar um livro
+    Route::delete('/books/{book}', [BookController::class, 'destroy']); // Excluir um livro
+
+});
 
