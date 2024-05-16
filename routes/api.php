2a03d8d8c4a0302d6df9c3dd4d39f8b642da6828
foxine <c.gc14@hotmail.com>
2024-05-16 12:50:28 -0300

    Sorry. I forgot about the Stores.

--- routes/api.php
+++ routes/api.php
@@ -19,11 +19,17 @@ Route::group([
         'middleware'=>['auth:sanctum']
 ], function (){
 
-    Route::get('/books', [BookController::class, 'index']); // Listar todos os livros
-    Route::post('/books', [BookController::class, 'store']); // Criar um novo livro
-    Route::get('/books/{book}', [BookController::class, 'show']); // Exibir um livro específico
-    Route::put('/books/{book}', [BookController::class, 'update']); // Atualizar um livro
-    Route::delete('/books/{book}', [BookController::class, 'destroy']); // Excluir um livro
+    Route::get('/books', [BookController::class, 'index']); 
+    Route::post('/books', [BookController::class, 'store']); 
+    Route::get('/books/{book}', [BookController::class, 'show']); 
+    Route::put('/books/{book}', [BookController::class, 'update']); 
+    Route::delete('/books/{book}', [BookController::class, 'destroy']); 
+    
+    Route::get('/stores', [StoreController::class, 'index']); 
+    Route::post('/stores', [StoreController::class, 'store']); 
+    Route::get('/stores/{store}', [BookController::class, 'show']); 
+    Route::put('/stores/{store}', [StoreController::class, 'update']); 
+    Route::delete('/stores/{store}', [StoreController::class, 'destroy']); 
 
 });
 
